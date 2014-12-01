cd12d578cee4fb8763143e37f3c5b25026398a9a
Nat Burns <nbaccount@burnskids.com>
2014-11-30 22:40:33 -0500

    Combine TLS connection function params into handler class.

--- lib/src/tls.dart
+++ lib/src/tls.dart
@@ -14,61 +14,22 @@ export 'gen/tls.dart';
 JsObject _tls() => context['forge']['tls'];
 final ForgeTls tls = new ForgeTls._();
 
-typedef bool Verifier(
-  TlsConnection connection,
-  bool verified,
-  int depth,
-  List<Certificate> chain
-);
-
-typedef void EventHandler(
-  TlsConnection connection
-);
-
-typedef String CertGetter(
-  TlsConnection connection,
-  Object hint
-);
-
-typedef String KeyGetter(
-  TlsConnection connection,
-  Certificate cert
-);
-
-typedef void HeartbeatReceiver(
-  TlsConnection connection,
-  ByteBuffer payload
-);
-
-typedef void ErrorHandler(
-  TlsConnection connection,
-  TlsError error
-);
-
 class ForgeTls {
   ForgeTls._();
 
-  TlsConnection createConnection({
-    bool server: false,
-    String sessionId: null,
-    Map<String, Object> sessionCache: null,
-    List<String> caStore: null, // or List<Certificate>
-    List<CipherSuite> cipherSuites: null,
-    String virtualHost: null,
-    Verifier verify: null,
-    Function verifyClient: null,
-    CertGetter getCertificate: null,
-    KeyGetter getPrivateKey: null,
-    Function getSignature: null,
-    Function deflate: null,
-    Function inflate: null,
-    EventHandler connected: null,
-    HeartbeatReceiver heartbeatReceived: null,
-    EventHandler tlsDataReady: null,
-    EventHandler dataReady: null,
-    EventHandler closed: null,
-    ErrorHandler error: null
-  }) {
+  TlsConnection createConnection(
+    TlsHandler handler,
+    {
+      bool server: false,
+      String sessionId: null,
+      Map<String, Object> sessionCache: null,
+      List<String> caStore: null, // or List<Certificate>
+      List<CipherSuite> cipherSuites: null,
+      String virtualHost: null,
+      bool verifyClient: false,
+      TlsCompressor compressor
+    }
+  ) {
     Map<String, Object> options = {
       'server': server,
       'sessionId': sessionId,
@@ -76,19 +37,23 @@ class ForgeTls {
       'caStore': caStore,
       'cipherSuites': cipherSuites,
       'virtualHost': virtualHost,
-      'verify': _verify(verify),
+      'verify': _verify(handler),
       'verifyClient': verifyClient,
-      'getCertificate': _getCertificate(getCertificate),
-      'getPrivateKey': _getPrivateKey(getPrivateKey),
-      'getSignature': getSignature,
-      'deflate': deflate,
-      'inflate': inflate,
-      'connected': _handler(connected),
-      'heartbeatReceived': _heartbeatReceived(heartbeatReceived),
-      'tlsDataReady': _handler(tlsDataReady),
-      'dataReady': _handler(dataReady),
-      'closed': _handler(closed),
-      'error': _error(error)
+      'getCertificate': _getCertificate(handler),
+      'getPrivateKey': _getPrivateKey(handler),
+      // 'getSignature': handler.getSignature,
+      'deflate': compressor != null
+        ? _reflate(compressor.deflate)
+        : null,
+      'inflate': compressor != null
+        ? _reflate(compressor.inflate)
+        : null,
+      'connected': _handler(handler.connected),
+      'heartbeatReceived': _heartbeatReceived(handler),
+      'tlsDataReady': _handler(handler.tlsDataReady),
+      'dataReady': _handler(handler.dataReady),
+      'closed': _handler(handler.closed),
+      'error': _error(handler)
     };
 
     return TlsConnection.$wrap(
@@ -99,48 +64,101 @@ class ForgeTls {
     );
   }
 
-  Function _verify(Verifier verifier)
+  Function _verify(TlsHandler handler)
     => (JsObject connection, bool verified, int depth, List<JsObject> chain)
-      => verifier(
+      => handler.verify(
         TlsConnection.$wrap(connection),
         verified,
         depth,
         new List.from(chain.map((cert) => Certificate.$wrap(cert)))
       );
 
-  Function _handler(EventHandler handler)
+  Function _handler(_EventHandler handler)
     => (JsObject connection)
       => handler(
         TlsConnection.$wrap(connection)
       );
 
-  Function _getCertificate(CertGetter getter)
+  Function _reflate(_Compressor reflater)
+    => (JsObject fragment)
+      => reflater(
+        ByteBuffer.$wrap(fragment)
+      );
+
+  Function _getCertificate(TlsHandler handler)
     => (JsObject connection, Object hint)
-      => getter(
+      => handler.getCertificate(
         TlsConnection.$wrap(connection),
         hint
       );
 
-  Function _getPrivateKey(KeyGetter getter)
+  Function _getPrivateKey(TlsHandler handler)
     => (JsObject connection, JsObject cert)
-      => getter(
+      => handler.getPrivateKey(
         TlsConnection.$wrap(connection),
         Certificate.$wrap(cert)
       );
 
-  Function _heartbeatReceived(HeartbeatReceiver receiver)
+  Function _heartbeatReceived(TlsHandler handler)
     => (JsObject connection, JsObject payload)
-      => receiver(
+      => handler.heartbeatReceived(
         TlsConnection.$wrap(connection),
         ByteBuffer.$wrap(payload)
       );
 
-  Function _error(ErrorHandler handler)
+  Function _error(TlsHandler handler)
     => (JsObject connection, JsObject error)
-      => handler(
+      => handler.error(
         TlsConnection.$wrap(connection),
         TlsError.$wrap(error)
       );
+}
+
+typedef void _EventHandler(
+  TlsConnection connection
+);
+
+abstract class TlsHandler {
+
+  bool verify(
+    TlsConnection connection,
+    bool verified,
+    int depth,
+    List<Certificate> chain
+  );
+
+  String getCertificate(
+    TlsConnection connection,
+    Object hint
+  );
+
+  String getPrivateKey(
+    TlsConnection connection,
+    Certificate cert
+  );
+
+  // String getSignature();
+
+  void connected(TlsConnection connection);
+
+  void heartbeatReceived(
+    TlsConnection connection,
+    ByteBuffer payload
+  );
+
+  void tlsDataReady(TlsConnection connection);
+  void dataReady(TlsConnection connection);
+  void closed(TlsConnection connection);
+  void error(TlsConnection connection, TlsError error);
+}
+
+typedef ByteBuffer _Compressor(
+  ByteBuffer data
+);
+
+abstract class TlsCompressor {
+  ByteBuffer deflate(ByteBuffer fragment);
+  ByteBuffer inflate(ByteBuffer fragment);
 }
 
 class CipherSuites {
--- test/tls_test.dart
+++ test/tls_test.dart
@@ -82,6 +82,73 @@ String certPem(Certificate cert) {
   return pem;
 }
 
+class ClientHandler extends TlsHandler {
+
+  final String certPem;
+  final String keyPem;
+
+  TlsConnection server;
+
+  ClientHandler(this.certPem, this.keyPem);
+
+  bool verify(TlsConnection c, bool verified, int depth, List<Certificate> certs) {
+    String cn = certs.first.subject.getField('CN').value;
+
+    logMessage('TLS Client verifying certificate w/CN: "$cn"');
+    logMessage('Verified: $verified...');
+
+    return verified;
+  }
+
+  void connected(TlsConnection c) {
+    logMessage('Client connected...');
+
+    new Future.delayed(
+      const Duration(milliseconds: 10),
+      () {
+        c.prepareHeartbeatRequest('heartbeat');
+        c.prepare('Hello Server');
+      }
+    );
+  }
+
+  String getCertificate(TlsConnection c, Object hint) {
+    logMessage('Client getting certificate...');
+    return certPem;
+  }
+
+  String getPrivateKey(TlsConnection c, Certificate cert) {
+    return keyPem;
+  }
+
+  void tlsDataReady(TlsConnection c) {
+    server.process(c.tlsData.getBytes());
+  }
+
+  void dataReady(TlsConnection c) {
+    var response = c.data.getBytes();
+    logMessage('Client received "$response"');
+    expect(response, equals('Hello Client'));
+    c.close();
+  }
+
+  void heartbeatReceived(TlsConnection c, ByteBuffer payload) {
+    logMessage('Client received heartbeat: ${payload.getBytes()}');
+  }
+
+  void closed(TlsConnection c) {
+    logMessage('Client disconnected.');
+  }
+
+  void error(TlsConnection c, TlsError error) {
+    logMessage('Client error: ${error.message}');
+  }
+
+  String getSignature() => '';
+  String deflate() => '';
+  String inflate() => '';
+}
+
 void runTests() {
   group('TLS', () {
     test('connects successfully', () {
@@ -98,72 +165,22 @@ void runTests() {
       KeyPair clientKeys = createKeys(clientCn);
       Certificate clientCert = createCert(clientKeys, clientCn);
 
-      String clientKeyPem = keyPem(clientKeys);
-      String clientCertPem = certPem(clientCert);
+      TlsHandler clientHandler = new ClientHandler(
+        certPem(clientCert),
+        keyPem(clientKeys)
+      );
 
       TlsConnection client, server;
 
       client = tls.createConnection(
+        clientHandler,
         server: false,
         caStore: [serverCertPem],
         cipherSuites: [
           CipherSuites.TLS_RSA_WITH_AES_128_CBC_SHA,
           CipherSuites.TLS_RSA_WITH_AES_256_CBC_SHA
         ],
-        virtualHost: 'server',
-
-        verify: (TlsConnection c, bool verified, int depth, List<Certificate> certs) {
-          String cn = certs.first.subject.getField('CN').value;
-
-          logMessage('TLS Client verifying certificate w/CN: "$cn"');
-          logMessage('Verified: $verified...');
-
-          return verified;
-        },
-
-        connected: (TlsConnection c) {
-          logMessage('Client connected...');
-
-          new Future.delayed(
-            const Duration(milliseconds: 10),
-            () {
-              c.prepareHeartbeatRequest('heartbeat');
-              c.prepare('Hello Server');
-            }
-          );
-        },
-
-        getCertificate: (TlsConnection c, Object hint) {
-          logMessage('Client getting certificate...');
-          return clientCertPem;
-        },
-
-        getPrivateKey: (TlsConnection c, Certificate cert) {
-          return clientKeyPem;
-        },
-
-        tlsDataReady: (TlsConnection c) {
-          server.process(c.tlsData.getBytes());
-        },
-
-        dataReady: (TlsConnection c) {
-          var response = c.data.getBytes();
-          logMessage('Client received "$response"');
-          expect(response, equals('Hello Client'));
-          c.close();
-        },
-
-        heartbeatReceived: (TlsConnection c, ByteBuffer payload) {
-          logMessage('Client received heartbeat: ${payload.getBytes()}');
-        },
-
-        closed: (TlsConnection c) {
-          logMessage('Client disconnected.');
-        },
-
-        error: (TlsConnection c, TlsError error) {
-          logMessage('Client error: ${error.message}');
-        }
+        virtualHost: 'server'
       );
     });
   });

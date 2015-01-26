c9b3370ec2bf9c3cabea86e51ed6dea621834b67
Nat Burns <nbaccount@burnskids.com>
2015-01-25 23:04:20 -0500

    Add constructors for ByteBuffer.

--- lib/src/gen/util.dart
+++ lib/src/gen/util.dart
@@ -1,4 +1,6 @@
 library forge.gen.util;
+
+import 'dart:typed_data' as types;
 import 'dart:js' as js;
 import 'package:js_wrapping/js_wrapping.dart' as jsw;
 
@@ -6,6 +8,16 @@ class ByteBuffer extends jsw.TypedJsObject {
   static ByteBuffer $wrap(js.JsObject jsObject) => jsObject == null ? null :
       new ByteBuffer.fromJsObject(jsObject);
   ByteBuffer.fromJsObject(js.JsObject jsObject) : super.fromJsObject(jsObject);
+  ByteBuffer.fromString(String data) : super(
+      js.context['forge']['util']['ByteBuffer'], [data]);
+
+  ByteBuffer.fromBuffer(types.ByteBuffer data) : super(
+      js.context['forge']['util']['ByteBuffer'], [jsw.jsify(data)]);
+
+  ByteBuffer.clone(ByteBuffer data) : super(
+      js.context['forge']['util']['ByteBuffer'], [data == null ? null : data.$unsafe]
+      );
+
   int length() => $unsafe.callMethod('length');
   bool isEmpty() => $unsafe.callMethod('isEmpty');
 
--- template/util.dart
+++ template/util.dart
@@ -1,9 +1,21 @@
 library forge.gen.util;
 
+import 'dart:typed_data' as types;
+
 import 'package:js_wrapping_generator/dart_generator.dart';
 
 @wrapper
+@Module('forge.util')
 abstract class ByteBuffer {
+  @generate
+  ByteBuffer.fromString(String data);
+
+  @generate
+  ByteBuffer.fromBuffer(types.ByteBuffer data);
+
+  @generate
+  ByteBuffer.clone(ByteBuffer data);
+
   int length();
   bool isEmpty();
 

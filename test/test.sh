17b827a8a3dfdd1eacfd5d26410b1cb9ff7dc072
Nat Burns <nbaccount@burnskids.com>
2015-03-08 21:57:32 -0400

    Add runner script for headless tests.

---
+++ test/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
+BROWSER_TEST_FILE=$DIR/test.html
+
+DUMP=$(DumpRenderTree $BROWSER_TEST_FILE)
+echo "$DUMP"
+
+REGEX="All [0-9]+ tests pass"
+
+if [[ $DUMP =~ $REGEX ]]
+then
+  echo Success!
+  exit 0
+else
+  echo Fail!
+  exit 1
+fi

4667ee44f5fff1500ffe75408ed85646ea9c1719
Nat Burns <nbaccount@burnskids.com>
2015-03-08 22:15:00 -0400

    Add download for content_shell and rename script.

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+which content_shell
+if [[ $? -ne 0 ]]; then
+  $DART_SDK/../chromium/download_contentshell.sh
+  unzip content_shell-linux-x64-release.zip
+
+  cs_path=$(ls -d drt-*)
+  PATH=$cs_path:$PATH
+fi
+
 DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 BROWSER_TEST_FILE=$DIR/test.html
 
